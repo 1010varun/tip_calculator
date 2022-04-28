01554ac89d02f17036d48504cbabc95c1bdc5768
1010varun <varunagarwal10.2@gmail.com>
2022-04-28 19:43:33 +0530

    tip_calculator(1)

---
+++ tip calculator/tip.js
@@ -0,0 +1,12 @@
+function submit(){
+    let amount = parseInt(document.getElementById("bill").value);
+    // amount = parseInt(amount);
+    // console.log(typeof amount);
+    let person = parseInt(document.getElementById("person").value);
+    // console.log(typeof person)
+    let tip = parseInt(document.getElementById("tip").value);
+    // console.log(tip)
+    let tip_amt = (amount + ((amount*tip)/100))/person;
+    console.log(tip_amt);
+    document.getElementById("share_person").value = tip_amt;
+}
